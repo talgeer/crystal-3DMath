ad06785ea4209d7ca9f9c1652b85a740bbf412d7
hdepoulp <henriette.depoulpiquet@gmail.com>
2017-05-14 17:37:06 -0400

    M reorganise \n M correct include

--- libs/vector/v4.cr
+++ libs/vector/v4.cr
@@ -1,195 +1,195 @@
-require "./V4Coord.cr"
+require "./v4Coord.cr"
 require "../utils/macroAccessor.cr"
 
 class V4
-	@data = [0.0, 0.0, 0.0, 1.0]
+  @data = [0.0, 0.0, 0.0, 1.0]
 
-	define_accessor(x, @data[0])
-	define_accessor(y, @data[1])
-	define_accessor(z, @data[2])
-	define_accessor(w, @data[3])
+  define_accessor(x, @data[0])
+  define_accessor(y, @data[1])
+  define_accessor(z, @data[2])
+  define_accessor(w, @data[3])
 
-	# constructor
-	def initialize(x = 0.0, y  = 0.0, z  = 0.0, w = 1.0)
-		@data[0] = x
-		@data[1] = y
-		@data[2] = z
-		@data[3] = w
-	end
+  # constructor
+  def initialize(x = 0.0, y = 0.0, z = 0.0, w = 1.0)
+    @data[0] = x
+    @data[1] = y
+    @data[2] = z
+    @data[3] = w
+  end
 
-	# Constructor by copy
-	def initialize(other : V4)
-		set(other.x, other.y, other.z, other.w)
-	end
+  # Constructor by copy
+  def initialize(other : V4)
+    set(other.x, other.y, other.z, other.w)
+  end
 
-	# set values one after another
-	def set(x, y, z, w)
-		@data[0] = x
-		@data[1] = y
-		@data[2] = z
-		@data[3] = w
-	end
+  # set values one after another
+  def set(x, y, z, w)
+    @data[0] = x
+    @data[1] = y
+    @data[2] = z
+    @data[3] = w
+  end
 
-	# addition operator
-	def +(other : V4)
-		V4.new(x + other.x, y + other.y, z + other.z, w + other.w)
-	end
+  # addition operator
+  def +(other : V4)
+    V4.new(x + other.x, y + other.y, z + other.z, w + other.w)
+  end
 
-	# substraction operator
-	def -(other : V4)
-		V4.new(x - other.x, y - other.y, z - other.z, w - other.w)
-	end
+  # substraction operator
+  def -(other : V4)
+    V4.new(x - other.x, y - other.y, z - other.z, w - other.w)
+  end
 
-	# inverse operator
-	def -
-		V4.new(-x, -y, -z, -w)
-	end
+  # inverse operator
+  def -
+    V4.new(-x, -y, -z, -w)
+  end
 
- 	#equals boolean
-  	def ==(other : V4)
-    	return (self.x == other.x) && (self.y == other.y) && (self.z == other.z) && (self.w == other.w)
-  	end
+  # equals boolean
+  def ==(other : V4)
+    return (self.x == other.x) && (self.y == other.y) && (self.z == other.z) && (self.w == other.w)
+  end
 
-  	# different boolean
-  	def !=(other : V4)
-    	!(self == other)
-  	end
+  # different boolean
+  def !=(other : V4)
+    !(self == other)
+  end
 
-  	# return a scaled vector
-  	def *(s : Number)
-  		result = V4.new(self)
-  	 	result.scale(s)
-  	 	return result
-  	end
- 
-	# scale the vector by a scalar
-	def scale(s : Number)
-		@data[0] *= s
-		@data[1] *= s
-		@data[2] *= s
-		@data[3] *= s
-	end
+  # return a scaled vector
+  def *(s : Number)
+    result = V4.new(self)
+    result.scale(s)
+    return result
+  end
 
-	# set one value using V4Coord
-	def set(mask : V4Coord, v : Number)
-		@data[mask.value] = v
-	end
+  # scale the vector by a scalar
+  def scale(s : Number)
+    @data[0] *= s
+    @data[1] *= s
+    @data[2] *= s
+    @data[3] *= s
+  end
 
-	# set several value using V4Coord
-	def sets(*values : Tuple(V4Coord, Number))
-		values.each do |tuple|
-			set tuple[0], tuple[1]
-		end
-	end
+  # set one value using V4Coord
+  def set(mask : V4Coord, v : Number)
+    @data[mask.value] = v
+  end
 
-	def get(mask : V4Coord)
-		@data[mask.value]
-	end
+  # set several value using V4Coord
+  def sets(*values : Tuple(V4Coord, Number))
+    values.each do |tuple|
+      set tuple[0], tuple[1]
+    end
+  end
 
-	def gets(*values : Tuple(V4Coord, Number))
-		values.each do |tuple|
-			tuple[1] = get(tuple[0])
-		end
-	end
+  def get(mask : V4Coord)
+    @data[mask.value]
+  end
 
-	# Cross product
-	def self.cross(v1 : V4, v2 : V4)
-		V4.new(
-			(v1.y * v2.z) - (v1.z * v2.y),
-			(v1.z * v2.x) - (v1.x * v2.z),
-			(v1.x * v2.y) - (v1.y * v2.x))
-	end
+  def gets(*values : Tuple(V4Coord, Number))
+    values.each do |tuple|
+      tuple[1] = get(tuple[0])
+    end
+  end
 
-	# Dot product on V3
-	def self.dot3(v1 : V4, v2 : V4)
-		v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
-	end
+  # Cross product
+  def self.cross(v1 : V4, v2 : V4)
+    V4.new(
+      (v1.y * v2.z) - (v1.z * v2.y),
+      (v1.z * v2.x) - (v1.x * v2.z),
+      (v1.x * v2.y) - (v1.y * v2.x))
+  end
 
-	# Dot product on V2
-	def self.dot2(v1 : V4, v2 : V4, coord1 = V4Coord::X, coord2 = V4Coord::Y)
-		v1.get(coord1) * v2.get(coord1) + v1.get(coord2) * v2.get(coord2)
-	end
+  # Dot product on V3
+  def self.dot3(v1 : V4, v2 : V4)
+    v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+  end
 
-	# Triple product on V3
-	def self.tripleProd(v1 : V4, v2 : V4, v3 : V4)
-		V4.dot3(V4.cross(v1, v2), v3)
-	end
+  # Dot product on V2
+  def self.dot2(v1 : V4, v2 : V4, coord1 = V4Coord::X, coord2 = V4Coord::Y)
+    v1.get(coord1) * v2.get(coord1) + v1.get(coord2) * v2.get(coord2)
+  end
 
-	#copy
-	def self.copy(origin : V4, dstination : V4)
-		destination.@data[0] = origin.@data[0]
-		destination.@data[1] = origin.@data[1]
-		destination.@data[2] = origin.@data[2]
-		destination.@data[3] = origin.@data[3]
-	end
+  # Triple product on V3
+  def self.tripleProd(v1 : V4, v2 : V4, v3 : V4)
+    V4.dot3(V4.cross(v1, v2), v3)
+  end
 
-	def self.copy(swizzleMask, origin : V4, destination : V4)
-		destination.set(0.0, 0.0, 0.0, 0.0)
-		i = 0
+  # copy
+  def self.copy(origin : V4, dstination : V4)
+    destination.@data[0] = origin.@data[0]
+    destination.@data[1] = origin.@data[1]
+    destination.@data[2] = origin.@data[2]
+    destination.@data[3] = origin.@data[3]
+  end
 
-		swizzleMask.each do |dimension|
-			destination.@data[i] = origin.get(dimension)
-			i += 1
-		end
-	end
+  def self.copy(swizzleMask, origin : V4, destination : V4)
+    destination.set(0.0, 0.0, 0.0, 0.0)
+    i = 0
 
-	def clone
-		V4.new x, y, z, w
-	end
+    swizzleMask.each do |dimension|
+      destination.@data[i] = origin.get(dimension)
+      i += 1
+    end
+  end
 
-	def clone(swizzleMask)
-		v = V4.new
-		i = 0
+  def clone
+    V4.new x, y, z, w
+  end
 
-		swizzleMask.each do |dimension|
-			v.@data[i] = get(dimension)
-			i += 1
-		end
-		v
-	end
+  def clone(swizzleMask)
+    v = V4.new
+    i = 0
 
-	#map
-	def map
-		v = V4.new
+    swizzleMask.each do |dimension|
+      v.@data[i] = get(dimension)
+      i += 1
+    end
+    v
+  end
 
-		v.@data[0] = yield @data[0]
-		v.@data[1] = yield @data[1]
-		v.@data[2] = yield @data[2]
-		v.@data[3] = yield @data[3]
+  # map
+  def map
+    v = V4.new
 
-		v
-	end
+    v.@data[0] = yield @data[0]
+    v.@data[1] = yield @data[1]
+    v.@data[2] = yield @data[2]
+    v.@data[3] = yield @data[3]
 
-	def map(mask)
-		v = V4.new
-		i = 0
-		mask.each do |dimension|
-			v.@data[i] = yield get(dimension)
-			i += 1
-		end
-		v
-	end
+    v
+  end
 
-	def reduce
-		a = 0.0
+  def map(mask)
+    v = V4.new
+    i = 0
+    mask.each do |dimension|
+      v.@data[i] = yield get(dimension)
+      i += 1
+    end
+    v
+  end
 
-		@data.each do |elt|
-			a += yield elt
-		end
+  def reduce
+    a = 0.0
 
-		a
-	end
+    @data.each do |elt|
+      a += yield elt
+    end
 
-	def reduce(mask)
-		a = 0.0
-		mask.each do |dimension|
-			a += yield get(dimension)
-		end
-		a
-	end
+    a
+  end
 
-	# debug
-	def to_s(io)
-		io << "[" << x << "," << y << "," << z << "," << w << "]"
-	end
+  def reduce(mask)
+    a = 0.0
+    mask.each do |dimension|
+      a += yield get(dimension)
+    end
+    a
+  end
+
+  # debug
+  def to_s(io)
+    io << "[" << x << "," << y << "," << z << "," << w << "]"
+  end
 end
--- src/main.cr
+++ src/main.cr
@@ -5,19 +5,29 @@ p2 = V4.new x: 1.0
 p3 = V4.new y: 1.0
 p4 = V4.new z: 1.0
 
+puts "initial vectors"
+puts "_______________________"
 puts "p1 :       #{p1}"
 puts "p2 :       #{p2}"
 puts "p3 :       #{p3}"
 puts "p4 :       #{p4}"
+puts "_______________________"
+puts ""
+puts "operators"
+puts "_______________________"
 puts "-p4 :      #{-p4}"
 puts "p4 + p3 :  #{p2 + p3}"
 puts "p4 - p3 :  #{p4 - p3}"
-puts "p2 * 4 :   #{p2.scale(4.0)}"
-
+puts "p2 * 4 :   #{p2 * (4.0)}"
+puts "p2.scale(4.0):   #{p2.scale(4.0)}"
+puts ""
+puts " setters"
+puts "_______________________"
 p2.set(1.0, 2.0, 3.0, 0.0)
 puts "p2 modif : #{p2}"
-
-p2.x(7.7)
+puts "p2.x(7.7) : #{p2.x(7.7)}"
+puts "-----------"
+puts "sets with masks"
 p2.sets({V4Coord::Y, 1.2}, {V4Coord::Z, 7.32})
 p2.set(V4Coord::W, 6.7)
 puts "p2 modif : #{p2}"
@@ -31,29 +41,48 @@ puts "p5.y #{p5.y}  #{p5.get(V4Coord::Y)}"
 puts "p5.z #{p5.z}  #{p5.get(V4Coord::Z)}"
 puts "p5.w #{p5.w}  #{p5.get(V4Coord::W)}"
 
-puts p5
-p7 = p5.map(swizzle(X, Y, Z)) {|a| (a + 1.14) * 6.3}
-p8 = p5.map {|a| (a + 1.14) * 6.3}
+puts " p5 #{p5}"
+p7 = p5.map(swizzle(X, Y, Z)) { |a| (a + 1.14) * 6.3 }
+puts "p7 #{p7}"
+p8 = p5.map { |a| (a + 1.14) * 6.3 }
+puts "p8 #{p8}"
+puts ""
+puts "copy"
+puts "_______________________"
+puts "clone"
 p6 = p5.clone
-
+puts "p6 #{p6}"
 p10 = V4.new
-
+puts "copy"
 V4.copy(swizzle(X, X, Z, Z), p6, p10)
 
-puts p10
-puts p7
-puts p8
-puts p6 == p5
-puts V4.cross(p6, p3)
+puts "p10 #{p10}"
+puts "p7 #{p7}"
+puts "p8 #{p8}"
+puts ""
+puts "complex operators"
+puts "_______________________"
+puts "p6 == p5 #{p6 == p5}"
+puts "p6 cross p3 #{V4.cross(p6, p3)}"
+puts ""
+puts "dot product"
+puts "--------"
 puts "p5 = #{p5}"
 puts "p4 = #{p4}"
-
+puts "p4 dot p5"
 puts V4.dot3(p4, p5)
+puts "p4 dot2 p5 (x,z)"
 puts V4.dot2(p4, p5, V4Coord::X, V4Coord::Z)
+puts "--------"
+puts "triple product p2 p3 p4"
 puts V4.tripleProd(p2, p3, p4)
-puts p5.reduce {|a| a}
-puts p7.reduce(swizzle(W)) {|a| a * a}
-puts p7.reduce(swizzle(Z)) {|a| a * a}
-puts p7.reduce(swizzle(X)) {|a| a * a}
-puts p7.reduce(swizzle(X, X)) {|a| a * a}
-puts p7.reduce {|a| a * a}
+puts ""
+puts "reduce"
+puts "_______________________"
+puts p5.reduce { |a| a }
+puts p7.reduce(swizzle(W)) { |a| a * a }
+puts p7.reduce(swizzle(Z)) { |a| a * a }
+puts p7.reduce(swizzle(X)) { |a| a * a }
+puts p7.reduce(swizzle(X, X)) { |a| a * a }
+puts p7.reduce { |a| a * a }
+puts "--------"
